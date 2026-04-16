0c2b61fd46b3b20a3f13792c7ea6e4818825efa2
Anjan Chakraborty <anjanchakraborty2023@gmail.com>
2026-04-17 00:12:51 +0530

    posts create-sucessfully

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,29 +1,51 @@
 const postmodel=require("../model/posts.model")
 const multer  = require('multer')
+const jwt=require("jsonwebtoken")
 
 const ImageKit =require("@imagekit/nodejs")
 
 const { toFile } = require("@imagekit/nodejs")
 
 
+
 const upload = multer({storage:multer.memoryStorage()})
 
 const client=new ImageKit({
-   
     privateKey:process.env.PRIVATE_KEY,
-    
 })
 
 const post=async(req,res)=>{
-    const caption =req.body
-    const imageUrl=req.file
+
+
+
+    const token=req.cookies.token
+    if(!token){
+        res.status(405).json({
+            message:"no valid token ! unauthorized access"
+        })
+    }
+    const decoded=jwt.verify(token,process.env.JWT_SECRET)
+
     const file= await client.files.upload({
         file:await toFile(req.file.buffer, "file"),
-        fileName:"test"
+        fileName:"test",
+        folder:"user-post-image"
     })
 
-    res.send(file)
+
+    const post= await postmodel.create({
+        caption:req.body.caption,
+        user:decoded.id,
+        ImageUrl:file.url
+    })
+
+    res.status(201).json({
+        message:"post created sucessfully",
+        post:post
+    })
+
 }
+
 
 
 
